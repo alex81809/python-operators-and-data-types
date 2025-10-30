57455ba9478f4883869a4d0a7fd14d1d11358388
Alexandra <asjlobaton@gmail.com>
2025-10-30 18:20:27 +0300

    Add operators.py to demonstrate variable types

---
+++ operators.py
@@ -0,0 +1,15 @@
+# integer type
+marks = 81
+print(type(marks))
+
+# float type
+cgpa = 3.333333
+print(type(cgpa))
+
+# boolean type
+admission = True
+print(type(admission))
+
+# string type
+string = "this is printed"
+print(type(string))
