31d6828b0f6e82c86a3637e3db29985b03e001b8
Alexandra <asjlobaton@gmail.com>
2025-10-30 18:35:29 +0300

    Create operators.py

---
+++ operators.py
@@ -0,0 +1,16 @@
+num1 = 20
+num2 = 3
+
+# Arithmetic
+floorDivision = num1 // num2
+print(floorDivision)
+
+remainder = num1 % num2 
+print(remainder)
+
+exponential = num1**num2 
+print(exponential)
+
+# Conditional
+print(num1 == num2)
+print(num1 != num2)
